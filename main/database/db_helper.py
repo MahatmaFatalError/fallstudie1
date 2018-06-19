a97910d844065884f290f27a305f43eb6ff8756e
Johannes Weber <johannesweber@users.noreply.github.com>
2018-06-19 22:29:31 +0200

    added filter to fetch_entity from DatastoreHelper

--- main/database/db_helper.py
+++ main/database/db_helper.py
@@ -29,6 +29,7 @@ class DatastoreHelper:
     def fetch_entity(self, entity_name, limit, offset):
         logger.info('Fetching from Offset: %s with Limit: %s', str(offset), str(limit))
         query = self.client.query(kind=entity_name)
+        query.add_filter('transported', '=', False)
         result = list(query.fetch(limit=limit, offset=offset))
         return result
 
