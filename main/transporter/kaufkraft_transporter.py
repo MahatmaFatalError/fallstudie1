a3e3286f95b36d39d460ec9001132c50abacf740
Tobias <tobias.rasbold@gmx.de>
2018-06-20 22:07:45 +0200

    add parse method to KaufkraftTransporter for filtering cities with %Region% and %rhein-kreis%

--- main/transporter/kaufkraft_transporter.py
+++ main/transporter/kaufkraft_transporter.py
@@ -6,13 +6,26 @@ from main.transporter.transporter import Transporter, logger
 
 class KaufkraftTransporter(Transporter):
 
+    @staticmethod
+    def parse(city_name):
+        result_city = ''
+        if ' ' in city_name:
+            split_city = city_name.split(' ')
+            for part in split_city:
+                if 'region' not in part.lower() and 'rhein-kreis' not in part.lower():
+                    result_city += part
+            return result_city
+        else:
+            return city_name
+
     def map(self, source_content):
         entities = []
         for item in source_content:
             city_name = item['city']
-            # TODO: region landkreis etc entfernen
+            # region landkreis etc entfernen
+            result_city = KaufkraftTransporter.parse(city_name)
             # \W = regex for any non word character
-            splits = re.compile('\W').split(city_name)
+            splits = re.compile('\W').split(result_city)
             city_name_for_query = '%'.join(splits)
             city_object_db = self.target_db.fetch_city_by_name(city_name_for_query)
             if city_object_db:
@@ -21,3 +34,4 @@ class KaufkraftTransporter(Transporter):
                 logger.debug(city_object_db)
             entities.append(city_object_db)
         return entities
+
