d0f93ce6cd83f4a45e32e47c81c982624a311566
Julian Ruppel <julian.ruppel@sap.com>
2018-06-22 18:56:32 +0200

    r script
    additional columns added to linear regression

--- main/analysis/restaurant_locations_germany.R
+++ main/analysis/restaurant_locations_germany.R
@@ -6,25 +6,38 @@ pg = dbDriver("PostgreSQL")
 con = dbConnect(pg, user="postgres", password="team123",
                 host="35.190.205.207", port=5432, dbname="fonethd")
 
+## create view restaurants_in_germany as (
+##  select r.*, c.name as category from restaurant r inner join food_category c on r.id = c.restaurant_id
+##  where city is not null)
+
 ## dtab = dbReadTable(con, "restaurant")
-dtab = dbGetQuery(con, "select * from restaurant where is_closed = false")
+dtab = dbGetQuery(con, "select * from restaurants_in_germany where is_closed = false")
 summary(dtab)
 str(dtab)
 
-## crunch data
+## crunch character data to factors
 priceCat = factor(dtab$price_range)
 str(priceCat)
 levels(priceCat)
 dtab$price_range = priceCat
 
+foodCat = factor(dtab$category)
+str(foodCat)
+levels(foodCat)
+dtab$category = foodCat
+
+cityCat = factor(dtab$city)
+dtab$city = cityCat
+stateCat = factor(dtab$state)
+dtab$state = stateCat
+
 ## linear Regression
-fit.lr <- lm(rating ~ price_range + longitude + latitude + review_count,
-              data=dtab )
+fit.lr <- lm(rating ~ city + state + review_count + category , data=dtab ) #+ price_range
 summary(fit.lr)
 
 ## tree
 library(partykit)
-fit.t <- ctree(rating ~ price_range + review_count, data=dtab)
+fit.t <- ctree(rating ~ price_range + city + review_count + category, data=dtab)
 plot(fit.t)
 
 
