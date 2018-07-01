b0aa718dcef21de52536e12fe07ef86f30fd0f3e
Johannes Weber <johannesweber@users.noreply.github.com>
2018-07-01 14:27:49 +0200

    added exception handling

--- main/cleaning/price_range.py
+++ main/cleaning/price_range.py
@@ -3,6 +3,7 @@ import logging
 from config import constants
 from main.database.db_helper import SqlHelper
 from main.helper import util
+from main.helper.exception import YelpError
 from main.helper.yelp import YelpHelper
 
 logger = logging.getLogger(__name__)
@@ -21,25 +22,31 @@ def main():
     result = db.fetch_entity_where('Restaurant', True, price_range=None)
     logger.info('Found {0} restaurants'.format(str(len(result))))
 
-    for restaurant in result:
-        restaurant_id = restaurant.id
-        business = yelp_helper.get_business(restaurant_id)
-        if 'price' in business:
-            price_range = business['price']
-            if price_range:
-                restaurant.price_range = price_range
+    try:
+        for restaurant in result:
+            restaurant_id = restaurant.id
+            business = yelp_helper.get_business(restaurant_id)
+            if 'error' not in business:
+                if 'price' in business:
+                    price_range = business['price']
+                    if price_range:
+                        restaurant.price_range = price_range
+                    else:
+                        logger.info('Price Range is null')
+                    restaurants.append(restaurant)
+                else:
+                    not_available_count += 1
             else:
-                logger.info('Price Range is null')
-            restaurants.append(restaurant)
-        else:
-            not_available_count += 1
-        logger.info(not_available_count)
-
-    for item in restaurants:
-        db.insert(item)
-
-    db.commit_session()
-    db.close_session()
+                raise YelpError(business['error']['code'], business['error']['description'])
+            logger.info(not_available_count)
+    except YelpError as error:
+        logger.exception(error)
+        logger.info('Adding {0} updated restaurants to DB...'.format(len(restaurants)))
+        for item in restaurants:
+            db.insert(item)
+        db.commit_session()
+    finally:
+        db.close_session()
 
 
 if __name__ == '__main__':
