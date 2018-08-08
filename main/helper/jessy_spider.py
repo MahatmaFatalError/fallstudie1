31594132736f105563092ae674b3c2d90fce5d7e
Johannes Weber <johannesweber@users.noreply.github.com>
2018-08-08 23:29:02 +0200

    finished RestaurantSpider

--- main/helper/jessy_spider.py
+++ main/helper/jessy_spider.py
@@ -26,7 +26,7 @@ class SpiderHelper:
         return result
 
 
-class RestaurantsSpider:
+class RestaurantSpider:
 
     def __init__(self, city):
         self.base_url = 'https://www.speisekarte.de'
@@ -34,22 +34,64 @@ class RestaurantsSpider:
         self.helper = SpiderHelper()
         self.restaurant_urls = []
         self.restaurant_trees = []
+        self.menu_urls = []
 
     def run(self):
         self._scrape_restaurants()
-        self._read_restaurant_urls()
+        self._scrape_restaurant_urls()
+        self._scrape_menu_urls()
+        self._build_menu_items_json()
 
+    def _build_menu_items_json(self):
+        restaurants = []
+
+        for urls in self.menu_urls:
+            restaurant = {
+                'id': None,
+                'address': None,
+                'menu': []
+            }
+            for url in urls:
+                if not restaurant['id']:
+                    restaurant_id = self.read_restaurtant_id(url)
+                    restaurant['id'] = restaurant_id
+                menu_tree = self.helper.scrape_page(url)
+                if not restaurant['address']:
+                    address = self.read_address(menu_tree)
+                    if address:
+                        restaurant['address'] = address
+                menu_items = self.read_menu_items(menu_tree)
+                if menu_items:
+                    category_id, category = self.read_menu_category(url)
+                    menu_category = {
+                        'id': category_id,
+                        'category': category,
+                        'menu_items': menu_items
+                    }
+                    restaurant['menu'].append(menu_category)
+
+            restaurants.append(restaurant)
+
+        all_restaurants = {
+            'total': len(restaurants),
+            'restaurants': restaurants
+        }
+        self.result = all_restaurants
+
+    def get_result_as_json(self):
+        print(self.result)
+        return json.dumps(self.result, ensure_ascii=False)
+
+    def get_result(self):
+        return self.result
+
+    def _scrape_menu_urls(self):
         for restaurant_url in self.restaurant_urls:
             search_url = restaurant_url + '/speisekarte'
             restaurant_tree = self.helper.scrape_page(search_url)
-            address = self.read_address(restaurant_tree)
-            logger.info("Restaurant Address: {0}".format(address))
-            menu_urls = self._get_menus(restaurant_tree)
-            for menu_url in menu_urls:
-                menu_tree = self.helper.scrape_page(menu_url)
-                menu_items = self._get_menu_items(menu_tree)
-                for item in menu_items:
-                    logger.info(item)
+            menu_urls = self.get_menus(restaurant_tree)
+            if menu_urls:
+                self.menu_urls.append(menu_urls)
 
     def _scrape_restaurants(self):
         restaurant_url_template = '/{city_name}/restaurants?page={page_number}'
@@ -63,7 +105,7 @@ class RestaurantsSpider:
             search_url = self.base_url + restaurant_url_template.format(city_name=self.city, page_number=page_number)
             tree = self.helper.scrape_page(search_url)
 
-    def _read_restaurant_urls(self):
+    def _scrape_restaurant_urls(self):
         for tree in self.restaurant_trees:
             search_results = tree.xpath('//div[@class="search-result"]')
             for result in search_results:
@@ -71,18 +113,38 @@ class RestaurantsSpider:
                 self.restaurant_urls.append(restaurant_url)
 
     @staticmethod
-    def _get_menu_items(tree):
-        menu_items = tree.xpath('//div[@class="menu-entry-filter"]/div/div/p/span/text()')
-        return menu_items
+    def read_menu_category(restaurant_url):
+        logger.info('Reading Category from {0}'.format(restaurant_url))
+        url_parts = restaurant_url.split('/')
+        category_string = url_parts[7]
+        category_parts = category_string.split('-')
+        category_id = category_parts[0]
+        category = category_parts[1]
+        cleaned_category = category.replace('_', ' ')
+        return category_id, cleaned_category.strip()
 
     @staticmethod
-    def _get_menus(tree):
+    def read_restaurtant_id(restaurant_url):
+        url_parts = restaurant_url.split('/')
+        restaurant_id = url_parts[5]
+        return restaurant_id
+
+    @staticmethod
+    def read_menu_items(tree):
+        items = []
+        menu_items = tree.xpath('//div[@class="menu-entry-filter"]/div/div/span/text()')
+        for item in menu_items:
+            if item:
+                items.append(item.strip())
+        return items
+
+    @staticmethod
+    def get_menus(tree):
         menu_urls = tree.xpath('//div[@class="card no-border"]/div/div/a/@href')
         return menu_urls
 
     @staticmethod
     def read_address(tree):
-        result = None
         address = {}
 
         name = tree.xpath('//p[@itemprop="name"]/text()')
@@ -95,20 +157,20 @@ class RestaurantsSpider:
             address['street'] = street[0]
             address['zip_code'] = zip_code[0]
             address['city'] = city[0]
-            result = json.dumps(address, ensure_ascii=False)
 
-        return result
+        return address
 
 
 if __name__ == '__main__':
     util.setup_logging()
 
-    city_name = 'mannheim'
-    spider = RestaurantsSpider(city_name)
+    city_name = 'schopfheim'
+    spider = RestaurantSpider(city_name)
     helper = SpiderHelper()
 
-    # tree = helper.scrape_page('https://www.speisekarte.de/mannheim/restaurant/friedrichsfelder_hof')
-    # address = spider.read_address(tree)
-    # print(address)
+    # tree = helper.scrape_page('https://www.speisekarte.de/schopfheim/restaurant/restaurant_hermes/speisekarte/1483436-vorspeisen_kalt_und_warm')
+    # print(spider.read_menu_items(tree))
 
     spider.run()
+    result = spider.get_result_as_json()
+    print(result)
