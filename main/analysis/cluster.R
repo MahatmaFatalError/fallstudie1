cb949bbfa44b0f2d167e2d2601e1ddda2b3ddb4c
Julian Ruppel <julian.ruppel@sap.com>
2018-08-12 23:32:53 +0200

    binary clustering

--- main/analysis/cluster.R
+++ main/analysis/cluster.R
@@ -11,29 +11,48 @@ con = dbConnect(pg, user="postgres", password="team123",
 data = dbGetQuery(con, "select * from restaurants_categories_flatter") # 68.734 rows
 summary(data)
 
-categoriesToCluster <- data[,-1]   # delete id
+categoriesToCluster <- data[,-1]   # get rid of id
 #categoriesToCluster <- na.omit(categoriesToCluster)
 str(categoriesToCluster)
 summary(categoriesToCluster)
+d1 <- replace(categoriesToCluster, is.na(categoriesToCluster), FALSE)
+d1 <- d1[0:10000,] # limit due to memory consumption
 
 require(cluster)
 #install.packages("ade4")
 library(ade4)
 
-# dist <- dist.binary(categoriesToCluster )
+## distances
+binary.dist.1 <- dist.binary(as.matrix(1 * (d1 > 0)), method=1 )
+binary.dist.2 <- dist.binary(as.matrix(1 * (d1 > 0)), method=2 )
+binary.dist.3 <- dist.binary(as.matrix(1 * (d1 > 0)), method=3 )
+binary.dist.4 <- dist.binary(as.matrix(1 * (d1 > 0)), method=4 )
+binary.dist.5 <- dist.binary(as.matrix(1 * (d1 > 0)), method=5 )
+binary.dist.6 <- dist.binary(as.matrix(1 * (d1 > 0)), method=6 )
+binary.dist.7 <- dist.binary(as.matrix(1 * (d1 > 0)), method=7 )
+binary.dist.8 <- dist.binary(as.matrix(1 * (d1 > 0)), method=8 )
+binary.dist.9 <- dist.binary(as.matrix(1 * (d1 > 0)), method=9 )
+binary.dist.10 <- dist.binary(as.matrix(1 * (d1 > 0)), method=10 )
 
-d1 <- replace(categoriesToCluster, is.na(categoriesToCluster), FALSE)
+distance = dist(d1, method = "binary")
 
-distance = dist(d1[0:10000,], method = "binary")
-
+## clustering 
 hc = hclust(distance)
+hc.centroid = hclust(binary.dist1, method="centroid")
+hc.ward <- hclust(binary.dist.10, method="ward.D")
 plot(hc)
+plot(hc.ward)
 
-group <- cutree(hc, k = 10)
-str(group)
-plot(hc, labels=group, xlab="")
 
-cluster.means = aggregate(data,by=list(cutree(hc, k = 6)), mean)
+## groups
+group <- cutree(hc.ward, k = 10)
+plot(group)
+plot(hc.ward, labels=group, xlab="")
+data$group <- group # error replacement has 10000 rows, data has 68734
+data.limited <- data[0:10000,] 
+data.limited$group <- group
+head(data.limited)
+#cluster.means = aggregate(data,by=list(cutree(hc.simplemathing, k = 10)), mean)
 
 
 # disconnect from the database
