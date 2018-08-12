b4c165e2759d8d8abd3b6caca32268a39b4f57f8
Julian Ruppel <julian.ruppel@sap.com>
2018-08-12 16:14:52 +0200

    binary clustering

--- main/analysis/cluster.R
+++ main/analysis/cluster.R
@@ -6,47 +6,34 @@ pg = dbDriver("PostgreSQL")
 con = dbConnect(pg, user="postgres", password="team123",
                 host="35.190.205.207", port=5432, dbname="fonethd")
 
-## dtab = dbReadTable(con, "restaurant")
-dtab = dbGetQuery(con, "select * from restaurants_final")
-summary(dtab)
-str(dtab)
+##### hclust for binary matrix
 
-## crunch character data to factors
-dtab$category1 = factor(dtab$category1)
-dtab$category2 = factor(dtab$category2)
-dtab$category3 = factor(dtab$category3)
+data = dbGetQuery(con, "select * from restaurants_categories_flatter") # 68.734 rows
+summary(data)
 
-categoriesToCluster <- dtab[,12:14]
+categoriesToCluster <- data[,-1]   # delete id
+#categoriesToCluster <- na.omit(categoriesToCluster)
+str(categoriesToCluster)
+summary(categoriesToCluster)
 
 require(cluster)
-#fit1 <- agnes(categoriesToCluster, method="ward") 
-#fit2 <- agnes(categoriesToCluster, method="average") 
-#fit3 <- agnes(categoriesToCluster, method="single") 
-#fit4 <- agnes(categoriesToCluster, method="complete")
+#install.packages("ade4")
+library(ade4)
 
-# https://medium.com/@anastasia.reusova/hierarchical-clustering-on-categorical-data-in-r-a27e578f2995
-gower.dist <- daisy(categoriesToCluster, metric = c("gower")) ## memory limitation
+# dist <- dist.binary(categoriesToCluster )
 
-divisive.clust <- diana(as.matrix(gower.dist), 
-                        diss = TRUE, keep.diss = TRUE)
-plot(divisive.clust, main = "Divisive")
+d1 <- replace(categoriesToCluster, is.na(categoriesToCluster), FALSE)
 
-aggl.clust.c <- hclust(gower.dist, method = "complete")
-plot(aggl.clust.c,
-     main = "Agglomerative, complete linkages")
+distance = dist(d1[0:10000,], method = "binary")
 
+hc = hclust(distance)
+plot(hc)
 
+group <- cutree(hc, k = 10)
+str(group)
+plot(hc, labels=group, xlab="")
 
-# https://dabblingwithdata.wordpress.com/2016/10/10/clustering-categorical-data-with-r/
-
-install.packages("klaR")
-library(klaR)
-cluster.results <-kmodes(categoriesToCluster, 6, iter.max = 10, weighted = FALSE ) ## Fehler in x[[jj]][iseq] <- vjj : Ersetzung hat Länge 0
-
-install.packages("cba")
-library(cba)
-cluster.results <-rockCluster(as.matrix(categoriesToCluster), 6 ) ## whole R session crashes
-cluster.output <- cbind(categoriesToCluster,cluster.results$cl)
+cluster.means = aggregate(data,by=list(cutree(hc, k = 6)), mean)
 
 
 # disconnect from the database
