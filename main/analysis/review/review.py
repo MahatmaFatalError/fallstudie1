5f4486d3bc0254519c900c7b5785e9b3c3f3eaf6
Johannes Weber <johannesweber@users.noreply.github.com>
2018-08-25 21:02:21 +0200

    added Tree Tagger and POS Tagging to review analysis script

--- main/analysis/review/review.py
+++ main/analysis/review/review.py
@@ -7,6 +7,7 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from collections import defaultdict
 from nltk.stem import SnowballStemmer
+import treetaggerwrapper
 
 from main.helper.yelp import YelpHelper
 
@@ -14,28 +15,29 @@ logger = logging.getLogger(__name__)
 
 # If you want to, change things here! #
 data_excerpt = []
-excerpt_size = 10000
+excerpt_size = 1000
 language = 'english'
-filename = 'yelp_academic_dataset_review.json'
+filename = 'yelp_academic_dataset_review_sample.json'
 reviews = None
+tree_tagger_dir = '../../../data/tree_tagger'
+reversed_result = False  # True = absteigend
 #####################################
 
 
 def run():
-    # TODO: improve algorithm with tagging etc?
     create_excerpt()
-    # enrich_with_category() # TODO: TEST!
+    # enrich_with_category()
     # write_json_file()
     create_df()
     one_star_reviews = reviews[reviews.stars == 1]
     five_star_reviews = reviews[reviews.stars == 5]
     logger.info('Analyzing 1-Star Reviews')
-    analyze(one_star_reviews)
+    analyze(one_star_reviews, '1-Star Reviews')
     logger.info('Analyzing 5-Star Reviews')
-    analyze(five_star_reviews)
+    analyze(five_star_reviews, '5-Star Reviews')
 
 
-def analyze(reviews_to_analyze):
+def analyze(reviews_to_analyze, file_name):
     top_words = defaultdict(float)
 
     bow_transformer = CountVectorizer(analyzer=text_process).fit(reviews_to_analyze['text'])
@@ -57,18 +59,35 @@ def analyze(reviews_to_analyze):
     for index in range(rows):
         feature_index = review_tfidf[index, :].nonzero()[1]
         tfidf_scores = zip(feature_index, [review_tfidf[index, x] for x in feature_index])
-        top5_scores_sorted_by_tfidf = sorted(tfidf_scores, key=lambda tup: tup[1], reverse=True)[:5]
+        top5_scores_sorted_by_tfidf = sorted(tfidf_scores, key=lambda tup: tup[1], reverse=reversed_result)[:5]
         for w, s in [(feature_names[i], s) for (i, s) in top5_scores_sorted_by_tfidf]:
             top_words[w] += s
 
-    logger.info('------------------------TOP 10 words in corpus----------------------------------')
-    top_words_sorted_by_tfidf = sorted(top_words.items(), key=lambda tup: tup[1], reverse=True)[:10]
-    for word, score in top_words_sorted_by_tfidf:
-        logger.info('{0} - {1}'.format(word, score))
+    write_result(top_words, file_name)
+
+
+def write_result(result, file_name):
+    if reversed_result:
+        title = file_name + '_reversed'
+    else:
+        title = file_name
+
+    title += '.txt'
+
+    with open(title, 'w') as result_file:
+        result_file.write(file_name)
+        result_file.write('\n')
+        result_file.write('------------------------ TOP 10 ----------------------------------')
+        result_file.write('\n')
+        result_sorted_by_tfidf = sorted(result.items(), key=lambda tup: tup[1], reverse=reversed_result)[:10]
+        for word, score in result_sorted_by_tfidf:
+            result_file.write('{0} - {1}'.format(word, score))
+            result_file.write('\n')
 
 
 def write_json_file():
     global data_excerpt
+
     with open('yelp_academic_dataset_review_sample.json', 'w') as outfile:
         for json_object in data_excerpt:
             json.dump(json_object, outfile)
@@ -76,26 +95,27 @@ def write_json_file():
 
 
 def text_process(mess):
-    """
-    Takes in a string of text, then performs the following:
-    1. Remove all punctuation
-    2. Remove all stopwords
-    3. Returns a list of the cleaned text
-    """
-    # Check characters to see if they are in punctuation
     global language
 
+    # Remove punctutation
     no_punc = [char for char in mess if char not in string.punctuation]
-
     # Join the characters again to form the string.
     no_punc = ''.join(no_punc)
 
-    # Now just remove any stopwords
+    # Remove any stopwords
     no_stopwords = [word for word in no_punc.split() if word.lower() not in stopwords.words(language)]
+
+    # Tag each word
+    tagged_words = tag_text(no_stopwords)
+
+    # Remove unwanted tags
+    extracted_tags = extract_tags(tagged_words)
+
     # Stem it
-    stemmer = SnowballStemmer(language)
-    stemmed_words = [stemmer.stem(word) for word in no_stopwords]
-    result = stemmed_words
+    # stemmer = SnowballStemmer(language)
+    # stemmed_words = [stemmer.stem(word) for word in tagged_words]
+
+    result = extracted_tags
     return result
 
 
@@ -142,6 +162,52 @@ def create_df():
 
     json_data = json.dumps(data_excerpt)
     reviews = pd.read_json(json_data)
+
+
+def tag_text(text):
+    if language == 'german':
+        country_code = 'de'
+    else:
+        country_code = 'en'
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG=country_code, TAGDIR=tree_tagger_dir)
+    text_with_tags = tagger.tag_text(text)
+
+    tags = treetaggerwrapper.make_tags(text_with_tags)
+    return tags
+
+
+def extract_tags(tags):
+    result = []
+    usefult_tags_german = [
+        'ADJA',
+        'ADJD',
+        'FM',
+        'NA',
+        'NE',
+        'NN'
+    ]
+    useful_tags_english = [
+        'FW',
+        'JJ',
+        'JJR',
+        'JJS',
+        'RB',
+        'RBS',
+        'RBR',
+        'NNS',
+        'NN',
+    ]
+    if language == 'german':
+        useful_tags = usefult_tags_german
+    else:
+        useful_tags = useful_tags_english
+
+    for tag in tags:
+        pos = tag.pos
+        if pos in useful_tags:
+            result.append(tag.word)
+
+    return result
 
 
 if __name__ == '__main__':
