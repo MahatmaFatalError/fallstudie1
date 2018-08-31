b5dfd23d804a1116058b599f1a5e5ea30a43e40c
Julian Ruppel <julian.ruppel@sap.com>
2018-08-31 22:27:25 +0200

    potenialanalys persistiert

--- main/analysis/potentialanalyse.R
+++ main/analysis/potentialanalyse.R
@@ -16,29 +16,37 @@ dtab = dbGetQuery(con, "select
                   count(id) / max(size_sqkm) restaurants_per_sqkm,
                   sum(review_count)/ max(size_sqkm) reviewcounts_per_sqkm,
                   percentile_cont(0.5) within group (order by rating ) median,
-                  min(rating), max(rating), mode() WITHIN GROUP (ORDER BY rating), stddev(rating) stddev,
+                  stddev(rating) stddev,
                   sum(review_count) sum_review_count,
                   sum(review_count) / count(id) reviewcounts_per_restaurant,
                   max(buying_power) bp,
                   max(size_sqkm) size_sqkm,
                   max(population_sqkm) population_sqkm
                   from restaurants_in_germany
-                  where review_count >= 9
+                  where review_count >= 9 and population_sqkm > 0
                   group by city
                   having sum(review_count)  > 138")
-summary(dtab)
-str(dtab)
+#summary(dtab)
+#str(dtab)
 
 dtab$z_restaurants_per_sqkm <- SoftMax(dtab$restaurants_per_sqkm)
 dtab$z_reviewcounts_per_restaurant <- SoftMax(dtab$reviewcounts_per_restaurant)
 dtab$z_avg_rating <- SoftMax(dtab$avg_rating)
-dtab$z_population_sqkm <- SoftMax(dtab$population_sqkm)
+dtab$z_population_restaurants_sqkm <- SoftMax(dtab$population_sqkm / dtab$restaurants_per_sqkm)
+#dtab$z_restaurants_population_sqkm <- SoftMax((dtab$restaurants_per_sqkm / dtab$population_sqkm))
+#((dtab$restaurants_per_sqkm / dtab$population_sqkm)) 
 
 #dtab$potential <- (0.2 * dtab$z_restaurants_per_sqkm) * (0.2 * dtab$z_reviewcounts_per_restaurant) * (0.6 * dtab$z_avg_rating)
-dtab$potential <- dtab$z_population_sqkm * (1-dtab$z_restaurants_per_sqkm) * (dtab$z_reviewcounts_per_restaurant) * (dtab$z_avg_rating)
+dtab$potential <- (dtab$z_population_restaurants_sqkm) * (dtab$z_reviewcounts_per_restaurant) * (dtab$z_avg_rating)
+# je höher z_population_restaurants_sqkm (also je mehr Einwohner/Restaurants) desto besser für unser potential
 
 View(arrange(dtab, desc(potential)))
+insert <- arrange(dtab[0:100, c(1, 16)], desc(potential))
+dbWriteTable(con, "top_cities", value = insert, row.names = FALSE) #append = TRUE
 
+# effect buying power mit price range, scatter plot // -> keine nennenswerte korrelation
+# TODO: Kontingenzanalyse chi^2 test lokale kategorieverteilung vs globale verteilung  -> top10_city_category
+# durchschnitts rating pro PLZ von den städten ->  top10_city_plz
 
 # disconnect from the database
 dbDisconnect(con)
---
+++ main/analysis/restaurant_locations_germany.R
@@ -0,0 +1,80 @@
+
+#library(ggplot2)
+#library(dplyr)
+library('RPostgreSQL')
+
+## connect to db
+pg = dbDriver("PostgreSQL")
+con = dbConnect(pg, user="postgres", password="team123",
+                host="35.190.205.207", port=5432, dbname="fonethd")
+
+## dtab = dbReadTable(con, "restaurant")
+restaurants_final = dbGetQuery(con, "select * from restaurants_final")
+summary(dtab)
+str(dtab)
+hist(dtab$buying_power)
+
+
+# categorize buying power into opgroups
+dtab$buying_power_groups <- cut(dtab$buying_power, breaks=c(-Inf, 20000, 24000, 28000, Inf), labels=c("low","middle", "upper middle","high"))
+plot(dtab$buying_power_groups)
+
+
+## crunch character data to factors
+priceCat = factor(dtab$price_range)
+#str(priceCat)
+#levels(priceCat)
+dtab$price_range = priceCat
+
+foodCat = factor(dtab$category)
+#str(foodCat)
+#levels(foodCat)
+dtab$category = foodCat
+
+dtab$category1 = factor(dtab$category1)
+dtab$category2 = factor(dtab$category2)
+dtab$category3 = factor(dtab$category3)
+
+cityCat = factor(dtab$city)
+dtab$city = cityCat
+dtab$state = factor(dtab$state)
+plot(dtab$state)
+
+## linear Regression
+fit.lr <- lm(rating ~ price_range + review_count + state + population_sqkm + buying_power_groups, data=dtab )
+#fit.lr <- lm(cbind(price_range,state,review_count,buying_power, population_sqkm, rent_avg) ~ rating, data=dtab )
+summary(fit.lr)
+
+## tree
+library(partykit)
+fit.t <- ctree(rating ~ price_range + state + review_count + buying_power + population_sqkm, data=dtab)
+plot(fit.t)
+
+
+library(effects)
+plot(effect("price_range", fit.lr))
+plot(effect("category", fit.lr))
+plot(effect("state", fit.lr))
+plot(effect("buying_power", fit.lr))
+plot(effect("population_sqkm", fit.lr))
+
+plot(dtab$review_count, dtab$rating)
+plot(restaurants_final$rating , restaurants_final$buying_power)
+plot(dtab$review_count, dtab$population_sqkm)
+
+
+require(corrplot)
+require(RColorBrewer)
+corrplot(dtab, type = "full", order = "hclust")
+
+# Beide sind normalverteilt, bedingung für pearson
+shapiro.test(restaurants_final$rating[1:5000])
+shapiro.test(restaurants_final$buying_power[1:5000])
+
+# ganz leichte minimale negative signifikante korrelation
+cor.test(restaurants_final$buying_power,restaurants_final$rating, method="pearson")
+
+View(dtab)
+
+# disconnect from the database
+dbDisconnect(con)
